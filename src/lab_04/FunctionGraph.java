373df19e44ea00960f6374ff05f8cf68e8ed181e
KAMIKOTOPO <daniilseluha51@gmail.com>
2023-08-12 20:24:00 +0300

    Сегодня очень мало сделал и немного криво

--- src/lab_04/FunctionGraph.java
+++ src/lab_04/FunctionGraph.java
@@ -1,21 +1,19 @@
 package lab_04;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class FunctionGraph {
-	public static void main(String[] args) {
-		menu();
-	}
-
-	public static void menu() {
+	public static void main(String[] args) throws IncorrectGraphDataException {
 		Scanner scr = new Scanner(System.in);
-
+		Graph graph = new Graph();
 		while (true)
 			try {
 				System.out.println();
 				System.out.println("""
 						Меню:
+						0)Задать ширину и высоту графика
 						1)Вывести таблицу 3 функций
 						2)Вывести график функции S1
 						3)Закончить программу
@@ -23,13 +21,16 @@ public class FunctionGraph {
 				System.out.print("Выберите одну из предложенных операций: ");
 				int x = scr.nextInt();
 				switch (x) {
+				case 0:
+					graph = sizeGraph();
+					break;
 				case 1:
-					printTable(partMenu());
+					partMenuTable(graph);
 					break;
 				case 2:
-					printGraph(partMenu2());
+					partMenuGraph(graph);
 					break;
-				case 3:
+				case 4:
 					System.out.println("Завершение программы...");
 					return;
 				default:
@@ -45,7 +46,10 @@ public class FunctionGraph {
 			}
 	}
 
-	public static Graph partMenu() throws IncorrectGraphDataException {
+	public static void partMenuTable(Graph gr) throws IncorrectGraphDataException {
+		if(Objects.isNull(gr)==false) {
+			throw new IncorrectGraphDataException("Вы не задали ширину и длину графика");
+		}
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Введите первое значение: ");
 		double first = scanner.nextDouble();
@@ -59,39 +63,32 @@ public class FunctionGraph {
 			step *= -1;
 			end = temp;
 		}
-		
-		return new Graph(first, step, end);
+		System.out.println(gr.printTableFunctions(first, step, end));
+
 	}
 
-	public static Graph partMenu2() throws IncorrectGraphDataException {
-		Scanner scr = new Scanner(System.in);
-		System.out.print("Введите первое значение: ");
-		double first = scr.nextDouble();
-		System.out.print("Введите последнее значение: ");
-		double end = scr.nextDouble();
-		return new Graph(first, end);
-	}
-
-	public static void printTable(Graph gr) {
-		System.out.println();
-		System.out.println(gr.printTableFunctions());
-	}
-
-	public static void printGraph(Graph gr) throws IncorrectGraphDataException {
+	public static void partMenuGraph(Graph gr) throws IncorrectGraphDataException {
 		Scanner scanner = new Scanner(System.in);
+		System.out.print("Введите первое значение: ");
+		double first = scanner.nextDouble();
+		System.out.print("Введите последнее значение: ");
+		double end = scanner.nextDouble();
 		System.out.print("Введите количество засечек от 4 до 8: ");
 		int serifs = scanner.nextInt();
 		System.out.println();
+		System.out.println(gr.printGraphFunctions(first, end, serifs));
+	}
+	public static Graph sizeGraph() throws IncorrectGraphDataException {
+		Scanner scanner = new Scanner(System.in);
 		System.out.print("Введите ширину графика: ");
 		int width = scanner.nextInt();
 		System.out.print("Введите высоту графика: ");
 		int height = scanner.nextInt();
-		System.out.println(gr.printGraphFunctions(serifs, width, height));
-	}
-	public static void sizeGraph() {
-		Scanner scanner = new Scanner(System.in);
-		
-		
+		if(width<=0||height<=0) {
+			throw new IncorrectGraphDataException("Ширина и высота должна быть больше нуля");
+		}
+		System.out.printf("\nВы задали ширину %d и высоту %d", width, height);
+		return new Graph(width, height);
 	}
 }
  
@@ -99,24 +96,17 @@ class Graph {
 	private double firstValue;
 	private double endValue;
 	private double step;
+	private int widht;
+	private int heigth;
 
-	public Graph(double firstValue, double step, double endValue) throws IncorrectGraphDataException {
 
-		this.firstValue = firstValue;
-		this.step = step;
-		this.endValue = endValue;
-		if (step <= 0) {
-			throw new IncorrectGraphDataException("\nС таким шагом нельзя создать таблицу");
-			}
+
+	public Graph() throws IncorrectGraphDataException {
+		
 	}
-
-	public Graph(double firstValue, double endValue) throws IncorrectGraphDataException {
-		this.firstValue = firstValue;
-		this.endValue = endValue;
-		if (firstValue <= 0 && endValue <= 0) {
-			throw new IncorrectGraphDataException(
-					"\nГрафика нет смысла выводить, так как на этом отрезке у функции нет значений");
-		}
+	public  Graph(int width, int height) {
+		this.widht = width;
+		this.heigth = height;
 	}
 
 	public double s1(double x) {
@@ -132,7 +122,10 @@ class Graph {
 		return Math.pow(Math.E, -(Math.abs(s1(x)) + Math.abs(s2(x))));
 	}
 
-	public StringBuilder printTableFunctions() {
+	public StringBuilder printTableFunctions(double firstValue, double step, double endValue) throws IncorrectGraphDataException {
+		if (step <= 0) {
+			throw new IncorrectGraphDataException("\nС таким шагом нельзя создать таблицу");
+			}
 		StringBuilder tableFunctions = new StringBuilder();
 
 		String sep = "+----------+----------+----------+----------+\n";
@@ -155,15 +148,19 @@ class Graph {
 
 
 	
-	public StringBuilder printGraphFunctions(int serifs, int width, int height) throws IncorrectGraphDataException {
+	public StringBuilder printGraphFunctions(double firstValue, double endValue, int serifs) throws IncorrectGraphDataException {
+		if (firstValue <= 0 && endValue <= 0) {
+			throw new IncorrectGraphDataException(
+					"\nГрафика нет смысла выводить, так как на этом отрезке у функции нет значений");
+		}
 		if (serifs > 8 || serifs < 4) {
 			throw new IncorrectGraphDataException("Введите пожалуйста засечку в диапозоне от 4 до 8");
 		}
-		if(width<=0||height<=0) {
-			throw new IncorrectGraphDataException("Ширина и высота должна быть больше нуля");
-		}
+		
 		StringBuilder graph = new StringBuilder();
-
+		int height = this.heigth;
+		int width = this.widht;
+		
 		char[][] plot = new char[height][width];
 		double minY = Integer.MAX_VALUE;
 		double maxY = Integer.MIN_VALUE;
@@ -197,8 +194,22 @@ class Graph {
 		
 		}
 		int numWidth = 6;
-		int spaceWidth = Math.abs((width - numWidth * serifs)) / (serifs - 1);
-		System.out.println(spaceWidth);
+		int realSerifs = width/numWidth;
+		if(realSerifs<serifs) {
+			if(realSerifs==1) {
+				serifs = 0;
+				System.out.printf("С такой шириной вы можете поместить максимум 1 засечку\n");
+			}
+			if(realSerifs<1) {
+				serifs = realSerifs;
+				System.out.printf("С такой шириной вы можете поместить максимум 1 засечку\n");
+			}
+			else {
+			serifs = realSerifs;
+			System.out.printf("С такой шириной вы можете поместить максимум %d засечек\n", realSerifs);
+			}
+		}
+		int spaceWidth = (width - numWidth * serifs)/ (serifs - 1);
 		int extraSpaces = (width - numWidth * serifs) % (serifs - 1);
 		double serifStep = (maxY - minY) / (serifs - 1);
 		graph.append(" ".repeat(8));
